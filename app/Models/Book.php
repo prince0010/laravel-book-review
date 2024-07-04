8b6b57743e4e60746782b4b37b9514db0fdd828d
PRINCE <princenagac12@gmail.com>
2024-07-04 11:09:32 +0800

    .

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,6 +16,15 @@ class Book extends Model
 
         return $this->hasMany(Review::class);
 
+    }
+
+    // Local Query Scope
+    // This can be used in the tinker by making the query in the tinker as a method 
+    // Like \App\Models\Book::where('title', 'LIKE', '%delectus%')->get(); 'delectus' is the title of the book
+    // to - with the Query Scope - to > \App\Models\Book::title('delectus')->get();
+    public function scopeTitle(Builder $query, $title) : Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 
 }
