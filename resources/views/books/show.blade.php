92db188f2288ac0043202831c086a2ffb4d1dda5
PRINCE <princenagac12@gmail.com>
2024-07-11 12:07:14 +0800

    Added Show Blade View

---
+++ resources/views/books/show.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="mb-4">
+        <!-- Book Title -->
+        <h1 class="sticky top-0 mb-2 text-2xl">
+            {{ $book->title }}
+        </h1>
+
+        <!-- Book Information -->
+        <div class="book-info">
+            <div class="book-author mb-4 text-lg font-semibold">
+                by {{ $book->author }}
+            </div>
+            <div class="book-rating flex items-center">
+                <div class="mr-2 text-sm font-medium text-slate-700">
+                    {{ number_format($book->review_avg_rating, 2) }}
+                </div>
+                <span class="book-review-count text-sm text-gray-500">
+                    {{ $book->review_count }} {{ Str::plural('review', $book->review_count ) }}
+                </span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <h2 class="mb-4 text-xl font-semibold">
+            Reviews
+        </h2>
+        <ul>
+            @forelse ($book->review as $review)
+            <li class="book-item mb-4">
+                <div>
+                    <div class="mb-2 flex items-center justify-between">
+                        <div class="semi-bold">{{ $review->rating }}</div>
+                        <div class="book-review-count">
+                            {{ $review->created_at->format('M j, Y') }}
+                        </div>
+                    </div>
+                    <p class="text-gray-700">{{$review->review}}</p>
+                </div>
+            </li>
+                
+            @empty
+            
+            <li class="mb-4">
+                <div class="empty-book-item">
+                    <p class="empty-text text-lg font-semibold">
+                        No Reviews Yet!
+                    </p>
+                </div>
+            </li>
+                
+            @endforelse
+        </ul>
+    </div>
+
+
+@endsection
